d3df3434867440dba7cd252f6ea6d65bb7d518c0
Sanjeev Reddy <sanjeev96red@gmail.com>
2018-12-19 03:28:45 -0600

    added handshake script

---
+++ handshake_latency/handshake.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+for ((i=0; i<100; i++)): do
+    curl -k https://www.google.com
+    curl -k https://www.youtube.com
+    curl -k https://www.facebook.com
+    curl -k https://www.baidu.com
+    curl -k https://www.google.co.in
+    curl -k https://vk.com
+    curl -k https://www.google.co.jp
+done
